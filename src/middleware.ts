caecab0bdb4a150243dbe0a884b914df12d001e3
Ryan Saunders <ryan.saunders@pocketrn.com>
2026-02-10 08:17:47 -0700

    moving proxy

--- src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { proxy, config } from '@/lib/proxy';
-import type { NextRequest } from 'next/server';
-
-export function middleware(req: NextRequest) {
-  return proxy(req);
-}
-
-export { config };
