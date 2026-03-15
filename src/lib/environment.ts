67699474305fcd0751e23505eba5fac27dcc6891
Ryan Saunders <ryan.saunders@pocketrn.com>
2026-03-15 16:07:17 -0600

    fix local

--- src/lib/environment.ts
+++ src/lib/environment.ts
@@ -1,2 +1,5 @@
 export const isProduction = process.env.NODE_ENV === 'production';
-export const isEmulated = process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATORS === 'true';
+export const isEmulated = (
+  process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATORS === 'true' ||
+  process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATORS === '1'
+);
