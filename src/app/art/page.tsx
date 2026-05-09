b124242fe0b13585c56809fde08c85c18abe226f
Ryan Saunders <ryan.saunders@pocketrn.com>
2026-05-09 09:44:00 -0600

    fix

--- src/app/art/page.tsx
+++ src/app/art/page.tsx
@@ -6,6 +6,7 @@ import { ArtDB } from '@/server/db/art.db';
 import { cacheLife, cacheTag } from 'next/cache';
 import DSButton from '@/components/ds/DSButton';
 import DSText from '@/components/ds/DSText';
+import { connection } from 'next/server';
 import { Suspense } from 'react';
 import DSSection from '@/components/ds/DSSection';
 
@@ -52,6 +53,7 @@ export default async function ArtPage() {
 }
 
 async function ArtPageData() {
+  await connection();
   const arts = await getArts();
 
   return arts.map((art) => (
